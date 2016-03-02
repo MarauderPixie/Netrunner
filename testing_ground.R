362e6b537152a70f16b4e8b6d78cdd6ec973e2e8
MarauderPixie <tobias.anton.85@gmail.com>
2016-03-02 18:50:03 +0100

    playing around with ploty

--- testing_ground.R
+++ testing_ground.R
@@ -1,29 +1,11 @@
-liga %>% 
-  filter(Seite == "Runner", Spieler != "Jannis") %>% 
-  gather(Systeme, Runzahl, Archiv, `R&D`, HQ, Remote) %>% 
-  ggplot(., aes(x = Spieler, y = Systeme, fill = Runzahl)) + 
-    geom_tile(color = "white", size = 0.1) +
-    labs(title = "Runs pro Spieler und System",
-         x = "",
-         y = "") +
-    scale_fill_viridis() +
-    theme_tufte() + 
-    theme(legend.position = "bottom")
-
-
-theme_few() +
-theme(title = element_text(size = 14),
-      axis.text    = element_text(size = 12),
-      legend.title = element_text(size = 12),
-      legend.text  = element_text(size = 10))
-
+## ggplotly testing
 
 plot <- score %>%
   group_by(Kalenderwoche, Seite) %>% 
   summarize("Wins" = sum(Punkte)/2) %>% 
   ggplot(., aes(x = Kalenderwoche, y = Wins, group = Seite, color = Seite)) +
+    geom_smooth(se = F) +
     geom_point() +
-    geom_smooth() +
     scale_color_brewer(palette = "Set1") +
     scale_x_continuous(breaks = 1:max(score$Kalenderwoche)) +
     scale_y_continuous(breaks = 1:13) +
@@ -32,27 +14,37 @@ plot <- score %>%
 ggplotly(plot)
 
 
+## the real plotly testing
 
-plot <- liga %>% 
-  filter(Seite == "Runner", Spieler != "Jannis") %>% 
-  gather(Systeme, Runzahl, Archiv, `R&D`, HQ, Remote) %>% 
-  ggplot(., aes(x = Spieler, y = Systeme, fill = Runzahl)) + 
-  geom_tile(color = "white", size = 0.4) +
-  labs(title = "Runs pro Spieler und System",
-       x = NULL, y = NULL) +
-  scale_fill_viridis(option = "B") +
-  cosmetics + 
-  theme(legend.position = "bottom")
+### kurvige graphen <3 (Seite)
+score %>% 
+  group_by(Kalenderwoche, Seite) %>% 
+  summarize("Wins" = sum(Punkte)/2) %>% 
+  plot_ly(., x = Kalenderwoche, y = Wins, line = list(shape = "spline"),
+          color = Seite, colors = c("blue", "red"), text = rownames(unique(Spieler)))
 
+### kurvige graphen <3 (Spieler)
+score %>% 
+  group_by(Kalenderwoche, Spieler) %>% 
+  summarize("Wins" = sum(Punkte)/2) %>% 
+  plot_ly(., x = Kalenderwoche, y = Wins, line = list(shape = "spline"),
+          color = Spieler, text = rownames(unique(Spieler)))
 
-
-liga %>% 
-  filter(Seite == "Konzern") %>% 
-  ggplot(aes(x = ID, fill = Fraktion)) +
-    geom_bar(color = "black") +
-    labs(x = "", y = "Spiele", legend = "") +
-    facet_grid(. ~ Fraktion, scales = "free") +
-    scale_fill_manual(values = c("darkorchid4", "red", "gold1", "darkolivegreen")) +
-    cosmetics +
-    theme(axis.text.x = element_text(angle = 25, hjust = 0, vjust = 1),
-          legend.position = "none")
+### kurvige graphen mit ID-Hovertext <3 (Spieler)
+score %>% 
+  group_by(Kalenderwoche, Spieler) %>% 
+  summarize("Wins" = sum(Punkte)/2,
+            "IDs"   = list(unique(ID))) %>% 
+  plot_ly(., x = Kalenderwoche, y = Wins, line = list(shape = "spline"),
+          color = Spieler, text = rownames(IDs))
+  
+  #### stellt sich raus: gar nicht so einfach
+  hover <- score %>% 
+            group_by(Kalenderwoche, Spieler) %>% 
+            summarize("Wins" = sum(Punkte)/2,
+                      "IDs"  = paste(unique(ID), sep = ",")) %>%
+            plot_ly(., x = Kalenderwoche, y = Wins, line = list(shape = "spline"),
+                    color = Spieler, text = str_replace_all(IDs, ",", "<br />"))
+    
+  str_replace_all(hover$IDs, ",", "\\n")
+  
