07b28312bb64dde422d02cecd39274357cef9b0c
MarauderPixie <tobias.anton.85@gmail.com>
2016-03-17 08:30:10 +0100

    does what it says it does

---
+++ one_massive_plot-chunk.R
@@ -0,0 +1,33 @@
+konzerne <- score %>% 
+  filter(Seite == "Konzern") %>% 
+  count(Fraktion)
+
+kon_ids  <- score %>% 
+  filter(Seite == "Konzern") %>% 
+  count(Fraktion, ID)
+
+
+hb <- paste(paste0(
+  filter(kon_ids, Fraktion == "Haas Bioroid")$n, "x"),
+  filter(kon_ids, Fraktion == "Haas Bioroid")$ID, collapse = ",")
+
+jt <- paste(paste0(
+  filter(kon_ids, Fraktion == "Jinteki")$n, "x"),
+  filter(kon_ids, Fraktion == "Jinteki")$ID, collapse = ",")
+
+nbn <- paste(paste0(
+  filter(kon_ids, Fraktion == "NBN")$n, "x"),
+  filter(kon_ids, Fraktion == "NBN")$ID, collapse = ",")
+
+wl <- paste(paste0(
+  filter(kon_ids, Fraktion == "Weyland Consortium")$n, "x"),
+  filter(kon_ids, Fraktion == "Weyland Consortium")$ID, collapse = ",")
+
+
+konzerne %>% 
+  mutate(p = n/sum(n)*100,
+         "IDs" = c(hb, jt, nbn, wl)) %>% 
+
+plot_ly(., x = Fraktion, y = p, name = "Fraktionen", type = "bar",
+        marker = list(color = c("darkorchid", "red", "gold", "darkolivegreen")),
+        text   = str_replace_all(IDs, ",", "<br />"))
