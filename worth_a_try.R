ffd003d85394602a35694648b7efb705a9a8c265
MarauderPixie <tobias.anton.85@gmail.com>
2016-03-17 09:10:10 +0100

    ...and see what happens

---
+++ worth_a_try.R
@@ -0,0 +1,30 @@
+plotlyfy <- function(df, x) {
+    paste(paste0(
+      filter(df, Fraktion == df$x)$n, "x"),
+      filter(df, Fraktion == df$x)$ID, collapse = ",")
+}
+
+
+
+
+c <- for (i in konzerne$Fraktion) {
+  paste(paste0(
+    filter(kon_ids, Fraktion == i)$n, "x"),
+    filter(kon_ids, Fraktion == i)$ID, collapse = ",")
+}
+
+
+
+konzerne %>% 
+  mutate(times_played = paste0(n, "x"),
+         )
+
+
+plotify <- function(data, var) {
+  data %>% 
+    group_by(Seite, Fraktion) %>% 
+    mutate(n = n(),
+           p = n / sum(n) * 100)
+    mutate(times = paste0(n, "x")) %>% 
+    summarize()
+}
