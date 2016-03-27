90669e51fb884c39a3637715f6ab97e1d0d50289
MarauderPixie <tobias.anton.85@gmail.com>
2016-03-27 23:11:18 +0200

    less bla

--- highcharter_vs_plotly.R
+++ highcharter_vs_plotly.R
@@ -9,25 +9,28 @@ plays <- score %>%
 list(plays$`Haas Bioroid`, plays$Jinteki, plays$NBN, plays$`Weyland Consortium`)
 
 ## better yet:
-bla <- score %>% 
+plays <- score %>% 
   filter(Seite == "Konzern") %>% 
   count(Fraktion, ID) %>% 
   mutate(plays = paste(paste0(n, "x"), ID),
          Reihe = 1:length(plays)) %>% 
-  summarize(n   = sum(n),
-            bla = paste(plays, collapse = "<br>"))
+  summarize(n     = sum(n),
+            plays = paste(plays, collapse = "<br>"))
 
 
 ## in highcharter:
 highchart() %>% 
-  hc_chart(type = "column") %>% 
+  # hc_chart(type = "column") %>% 
   hc_title(text = "Konzern Fraktionen") %>% 
-  hc_xAxis(categories = bla$Fraktion) %>%
+  hc_xAxis(categories = plays$Fraktion) %>%
   hc_yAxis(title = list(text = "Spiele")) %>% 
-  hc_add_series(data = bla$n, name = "Spiele", colorByPoint = TRUE) %>% 
+  hc_add_series(data = plays$n, name = "Spiele", colorByPoint = TRUE, type = "column") %>% 
+  hc_add_series(data = plays$plays, type = NULL) %>% 
   hc_tooltip(headerFormat = "<b>{point.key}:</b> <br> <table>",
-             pointFormat = "plays$`Haas Bioroid`, plays$Jinteki, plays$NBN, plays$`Weyland Consortium` <br>",
-             footerFormat = "<b>Gesamt:</b> {point.y} Spiele <table />") %>% 
+             # pointFormat = "{plays.point.y}",
+             footerFormat = "<br><b>Gesamt:</b> {point.y} Spiele </table>",
+             #valueSuffix = plays$plays, 
+             shared = F) %>% 
   hc_legend(enabled = FALSE) %>% 
   hc_colors(colors = kon_cols)
 
