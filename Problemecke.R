dd3188ed1aaed7973d332e4335067c1ef0ddff16
MarauderPixie <tobias.anton.85@gmail.com>
2016-05-17 23:27:53 +0200

    solved games overview. sort of.

--- Problemecke.R
+++ Problemecke.R
@@ -1,27 +1,3 @@
-# Heatmaps of games played against whom
-
-protoplays <- as.data.frame.matrix(crossprod(table(score$Spiel, score$Spieler)))
-
-hs_plays <- protoplays %>% 
-  gather(Spieler, Spiele, Bjarne, Bodo, Falk, Jan, 
-         Johannes, Josh, `Josh R.`, Kai, Paul, Tobias) %>% 
-  mutate(Gegner = rep(c("Bjarne", "Bodo", "Falk", "Jan",
-                        "Johannes", "Josh", "`Josh R.`", 
-                        "Kai", "Paul", "Tobias"), 10))
-
-
-## Test
-ggplot(hs_plays, aes(x = Spieler, y = Gegner, fill = Spiele)) + 
-  geom_tile(color = "white", size = 0.4)
-
-## filter player == Gegner
-
-if {Spieler == Gegner} {
-  
-}
-
-
-
 # beim gathern der systeme entstehen mehr n als es tatsächlich gibt;
 # Problem: means zB fallen zu klein aus
 systeme <- runner_liga %>%
