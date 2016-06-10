d8a8897bae4a7d53b93cfc9f8226e6e6496a9979
MarauderPixie <tobias.anton.85@gmail.com>
2016-06-10 10:54:11 +0200

    messing around again

--- highcharter_vs_plotly.R
+++ highcharter_vs_plotly.R
@@ -1,5 +1,5 @@
 # tooltip dataframe
-plays <- score %>% 
+id_plays <- score %>% 
   filter(Seite == "Konzern") %>% 
   count(Fraktion, ID) %>% 
   transmute(plays = paste(paste0(n, "x"), ID),
@@ -9,7 +9,7 @@ plays <- score %>%
 list(plays$`Haas Bioroid`, plays$Jinteki, plays$NBN, plays$`Weyland Consortium`)
 
 ## better yet:
-plays <- score %>% 
+id_plays <- score %>% 
   filter(Seite == "Konzern") %>% 
   count(Fraktion, ID) %>% 
   mutate(plays = paste(paste0(n, "x"), ID),
@@ -36,8 +36,39 @@ highchart() %>%
 
 
 ## in plotly:
-  plot_ly(bla, x = Fraktion, y = n, name = "Fraktionen", type = "bar",
+  plot_ly(id_plays, x = Fraktion, y = n, name = "Fraktionen", type = "bar",
           marker = list(color = c("darkorchid", "red", "gold", "darkolivegreen")),
-          text   = bla)
+          text   = plays)
 
-text   = str_replace_all(IDs, ",", "<br />"))
+text   = str_replace_all(IDs, ",", "<br />")
+
+  ggplotly(
+    ggplot(score, aes(x = Fraktion, fill = Fraktion)) +
+      geom_bar(color = faction_cols, alpha = 0.6) +
+      labs(x = "", y = "Spiele") +
+      facet_grid(. ~ Seite, scales = "free_x") +
+      faction_fill + cosmetics +
+      theme(legend.position = "none")
+  )
+  
+  
+## fancy polar charts in plotly:
+  p <- plot_ly(plotly::wind, r = r, t = t, color = nms, type = "area") %>% 
+  layout(., radialaxis = list(ticksuffix = "%"), orientation = 270)
+  
+  sys_runs <- runner_liga %>% 
+    filter(Spieler != "Jannis", Spieler != "Jan") %>% 
+    gather(Systeme, Sysruns, Archiv, `R&D`, HQ, Remote) %>% 
+    group_by(Spieler, Systeme) %>% 
+    summarize(Sysruns = sum(Sysruns, na.rm = T)) %>% 
+    ggplot(aes(x = Spieler, y = Systeme, fill = Sysruns)) + 
+      geom_tile(color = "white", size = 0.4) +
+      labs(title = "Runs pro Spieler und System",
+           x = NULL, y = NULL) +
+      scale_fill_viridis(option = "D") +
+      cosmetics + 
+      theme(legend.position = "bottom")
+  
+  plot_ly(sys_runs, r = Sysruns, t = Systeme, color = Spieler, type = "area",
+          opacity = 0.7, name = "Systeme") 
+
