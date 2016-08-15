cb0403562f75bcc34ce21748299b1a914b85ff46
MarauderPixie <tobias.anton.85@gmail.com>
2016-08-15 10:14:41 +0200

    more playing around

--- rbokeh.R
+++ rbokeh.R
@@ -9,4 +9,15 @@ figure() %>%
 figure(title = "Häufigkeiten pro Fraktion", ylab = "Anzahl", xlab = NULL,
        xgrid = F, tools = NULL, legend_location = NULL) %>% 
   ly_bar(Fraktion, data = liga, color = Fraktion, alpha = 0.95,
-         hover = list(Fraktion, ID), fill_color = cols, line_color = "black")
+         hover = ID, fill_color = cols, line_color = "black")
+
+
+mittelrun <- runner_liga %>% group_by(Spieler) %>% summarize(Mittel = mean(Runs))
+
+figure(title = "Runs pro Spieler", ylab = "Runs", xlab = NULL, tools = NULL,
+       xgrid = F, ygrid = F, legend_location = NULL) %>% 
+  ly_lines(x = Spieler, y = mean(runner_liga$Runs), data = runner_liga,
+           color = "gray", alpha = 0.8) %>% 
+  ly_points(Spieler, Runs, data = runner_liga, hover = "als @ID gegen @Gegner",
+            glyph = "diamond", color = Fraktion, size = 15) %>% 
+  ly_points(Spieler, Mittel, data = mittelrun, glyph = "x", color = Spieler, size = 20)
