44d6e036e7c904116c4ca2a6eec7ec98b583a29b
MarauderPixie <tobias.anton.85@gmail.com>
2016-09-02 23:21:42 +0200

    becoming a builder

--- build.R
+++ build.R
@@ -1,12 +1,21 @@
+#! /usr/bin/env Rscript
+rmarkdown::render_site(input = ".")
+
 # Collect output files
 
-out_docs   <- c(list.files(pattern = "*.html"))
-out_assets <- c("assets", "highscore-plots", "liga-plots")
+out_docs   <- paste0("./_site/", c(list.files(path = "_site/", pattern = "*.html")))
+out_assets <- c("assets", "_site/site_libs")
 
 
 # Copy files to btsync directory
 
-out_dir <- "~/Dokumente/btsync/tobi.tadaa-data.de/Netrunner"
+out_dir <- "~/Dokumente/btsync/tobi.tadaa-data.de/Netrunner/"
 
 sapply(out_docs,   file.copy, to = out_dir, overwrite = T, recursive = F)
 sapply(out_assets, file.copy, to = out_dir, overwrite = T, recursive = T)
+
+
+# since everything works fine, this isn't of any use for now,
+# I'll keeo it just in case anything breaks
+
+# system(command = "cp -r ./_site/ ~/Dokumente/btsync/tobi.tadaa-data.de/Netrunner/")
